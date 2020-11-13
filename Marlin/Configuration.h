e666033cde10649999da61c98b5987fe0658d33c
Ralf Vogler <ralf.vogler@gmail.com>
2020-11-13 14:31:56 +0100

    tried FAN_SOFT_PWM: no more PWM whine, but now on/off clatter at ~100Hz

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2391,12 +2391,12 @@
 //#define NUM_M106_FANS 1
 
 // Increase the FAN PWM frequency. Removes the PWM noise but increases heating in the FET/Arduino
-#define FAST_PWM_FAN
+//#define FAST_PWM_FAN
 
 // Use software PWM to drive the fan, as for the heaters. This uses a very low frequency
 // which is not as annoying as with the hardware PWM. On the other hand, if this frequency
 // is too low, you should also increment SOFT_PWM_SCALE.
-//#define FAN_SOFT_PWM
+#define FAN_SOFT_PWM
 
 // Incrementing this by 1 will double the software PWM frequency,
 // affecting heaters, and the fan if FAN_SOFT_PWM is enabled.
